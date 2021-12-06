606e74477b9186ec6579d559249b8f3cdc84e9d5
space thyme <spacethyme@shaw.ca>
2021-12-05 21:04:03 -0800

    begin css for form

--- src/IntakeForm.js
+++ src/IntakeForm.js
@@ -3,15 +3,15 @@ import { Link } from "react-location";
 function IntakeFormTextField({ field, display }) {
     return (
         <div className="input-container">
-        <label id={`${field}-label`} htmlFor={field}>{display}</label>
-        <input 
-            type="text"
-            id={field}
-            name={field}
-            placeholder={display}
-            aria-labelledby={`${field}-label`}
-        />
-    </div>
+            <label id={`${field}-label`} htmlFor={field}>{display}</label>
+            <input 
+                type="text"
+                id={field}
+                name={field}
+                placeholder={display}
+                aria-labelledby={`${field}-label`}
+            />
+        </div>
     )
 }
 
@@ -19,13 +19,13 @@ function IntakeFormCheckboxField({ field, display }) {
     return (
         <>
             <label id={`${field}-label`} htmlFor={field}>
-            <input
-                type="checkbox"
-                value={display}
-                id={field}
-                name={field}
-            />
-            {display}
+                <input
+                    type="checkbox"
+                    value={display}
+                    id={field}
+                    name={field}
+                />
+                {display}
             </label>
         </>
     )
@@ -34,11 +34,11 @@ function IntakeFormCheckboxField({ field, display }) {
 export default function IntakeForm() {
     return (
         <div className="flex-wrapper">
-            <div className="logo-and-name">
+            <section className="logo-and-name">
                 <h1>DevCard</h1>
                 <p>Your personal digital portfolio</p>
-            </div>
-            <div className="intake-form">
+            </section>
+            <section className="intake-form">
                 <form>
                     <h2>Create your DevCard</h2>
 
@@ -62,13 +62,7 @@ export default function IntakeForm() {
                     </button>
                     
                 </form>
-
-                <p>Sample Links for Later:</p>
-                <ul>
-                    <li><Link to="/1">(go to Profile #1)</Link></li>
-                    <li><Link to="/2">(go to Profile #2)</Link></li>
-                </ul>
-            </div>
+            </section>
         </div>
     )
 }
